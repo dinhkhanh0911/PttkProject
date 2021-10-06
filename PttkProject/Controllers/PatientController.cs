cc40b6095dad457401bccb899e6aa7edda9f4c71
Khánh Đinh <dinhvankhanh102@gmail.com>
2021-10-07 04:10:55 +0700

    Add project files.

---
+++ PttkProject/Controllers/PatientController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PttkProject.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ImportInformation()
+        {
+            return View();
+        }
+        public ActionResult UpdateInformation()
+        {
+            return View();
+        }
+        public ActionResult SearchPatient()
+        {
+            return View();
+        }
+        public ActionResult ImportMedicalRecord()
+        {
+            return View();
+        }
+        public ActionResult UpdateMedicalRecord()
+        {
+            return View();
+        }
+    }
+}
