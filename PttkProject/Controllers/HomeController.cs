45f0b32cebbd38788dbaa92cea33589b3c21d5ff
toan <toankt2k@gmail.com>
2021-11-03 13:42:56 +0700

    pull

--- PttkProject/Controllers/HomeController.cs
+++ PttkProject/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,10 +13,43 @@ namespace PttkProject.Controllers
     {
         public ActionResult Index()
         {
-            
+            getdataAsync();
             return View();
         }
 
-        
+        public async Task getdataAsync()
+        {
+            try
+            {
+                string url = "https://emoh.moh.gov.vn/api/khamchuabenh/tiem_nhan_kcb_xml";
+                string tinh = "https://provinces.open-api.vn/api/p/";
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://emoh.moh.gov.vn/api/tra-cuu-hoso-kcb?");
+
+                var Items = new List<Items>();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string categories = await response.Content.ReadAsStringAsync();
+                    var ss = JsonConvert.DeserializeObject(categories, new List<Items>().GetType());
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        [Serializable]
+        class Items
+        {
+            public string name;
+            public int code;
+            public string division_type;
+            public string codename;
+            public string phone_code;
+            public object[] districts;
+
+        }
     }
 }
