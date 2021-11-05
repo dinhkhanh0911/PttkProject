fbbf7aa0e46b91fac6d08ab817c5e01191c31a05
quyetkaito <quyetkaito@gmail.com>
2021-11-05 14:12:20 +0700

    them phong benh

--- PttkProject/Controllers/PhongBenhController.cs
+++ PttkProject/Controllers/PhongBenhController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DBCovid.models;
+using PttkProject.DatabaseDAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,26 +10,52 @@ namespace PttkProject.Controllers
 {
     public class PhongBenhController : Controller
     {
+        private PhongBenhDAO phongbenhDAO = new PhongBenhDAO();
         // GET: Room
         public ActionResult Index()
         {
             return View();
         }
+        /*view thêm phòng bệnh*/
         public ActionResult themphong()
         {
+            setViewBagLoaiPhong();
             return View();
         }
+        /*thêm phòng bệnh*/
+        [HttpPost]
+        public ActionResult themPhong(PhongBenh pb)
+        {
+            try
+            {
+                phongbenhDAO.themPhongBenh(pb);
+                return RedirectToAction("Index", "PhongBenh");
+
+            }
+            catch (Exception e)
+            {
+                //lloi
+                return RedirectToAction("Index", "PhongBenh");
+            }
+        }
+
+
         public ActionResult suaphong()
         {
             return View();
         }
-        public ActionResult deleteRoom()
+        public ActionResult xoaPhong()
         {
             return View();
         }
         public ActionResult timkiemphong()
         {
             return View();
+        }
+        private void setViewBagLoaiPhong()
+        {
+            List<LoaiPhong> l = phongbenhDAO.layDSLoaiPhong();
+            ViewBag.loaiPhong = new SelectList(l, "ID", "tenLoaiPhong");
         }
     }
 }
--- PttkProject/DatabaseDAO/PhongBenhDAO.cs
+++ PttkProject/DatabaseDAO/PhongBenhDAO.cs
@@ -1,4 +1,5 @@
 ﻿using DBCovid.Data;
+using DBCovid.models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,34 @@ namespace PttkProject.DatabaseDAO
 {
     public class PhongBenhDAO: DBCovidContext
     {
+        private DBCovidContext data = new DBCovidContext();
+        public List<PhongBenh> layDSPhongBenh()
+        {
+            List<PhongBenh> p = phongBenh.ToList();
+            return p;
+        }
+        public List<PhongBenh> layDSPhongBenh(int loaiPhongID)
+        {
+            List<PhongBenh> p = phongBenh.Where(x => x.loaiPhongID == loaiPhongID).ToList();
+            return p;
+        }
+        public List<LoaiPhong> layDSLoaiPhong()
+        {
+            List<LoaiPhong> l = loaiPhong.ToList();
+            return l;
+        }
+
+        public bool themPhongBenh(PhongBenh pb)
+        {
+            try
+            {         
+                data.phongBenh.Add(pb);
+                data.SaveChanges();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
