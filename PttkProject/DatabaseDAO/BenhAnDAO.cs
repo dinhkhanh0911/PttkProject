f02e8bb0c8b1c8dfd18915f1b5f7ba012ed37fc8
toan <toankt2k@gmail.com>
2021-11-11 15:06:50 +0700

    a

--- PttkProject/DatabaseDAO/BenhAnDAO.cs
+++ PttkProject/DatabaseDAO/BenhAnDAO.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using DBCovid.Data;
+using DBCovid.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace PttkProject.DatabaseDAO
 {
-    public class BenhAnDAO
+    public class BenhAnDAO:DBCovidContext
     {
+        public BenhAn layBenhAnMoiNhat(int idBenhNhan)
+        {
+            try
+            {
+                BenhAn BA = (from ba in benhAn
+                             join tt in trangThai
+                             on ba.trangThaiID equals tt.ID
+                             where tt.ID == 1
+                             select ba).FirstOrDefault();
+                return BA;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
