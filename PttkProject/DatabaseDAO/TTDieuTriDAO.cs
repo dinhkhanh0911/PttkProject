94fccb6690ed861b84a6b35ee27a2800cad8a3ea
quyetkaito <quyetkaito@gmail.com>
2021-11-16 19:51:33 +0700

    add some DAO

---
+++ PttkProject/DatabaseDAO/TTDieuTriDAO.cs
@@ -0,0 +1,55 @@
+﻿using DBCovid.Data;
+using DBCovid.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PttkProject.DatabaseDAO
+{
+    public class TTDieuTri : DBCovidContext
+    {
+        /* Thông tin điều trị */
+        public bool themThongTinDieuTri(ThongTinDieuTri t)
+        {
+            try
+            {
+                thongtinDieuTri.Add(t);
+                SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public List<ThongTinDieuTri> layDSThongTinDieuTri(int benhAnID)
+        {
+            List<ThongTinDieuTri> list;
+            try
+            {
+                list = thongtinDieuTri.Where(t => t.benhAnID == benhAnID).ToList();
+                return list;
+            }
+            catch (Exception e)
+            {
+                list = new List<ThongTinDieuTri>();
+                return list;
+            }
+        }
+        public bool xoaThongTinDieuTri(int ID)
+        {
+            try
+            {
+                ThongTinDieuTri t = thongtinDieuTri.Where(x => x.ID == ID).FirstOrDefault();
+                thongtinDieuTri.Remove(t);
+                SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PttkProject/DatabaseDAO/TTTruyVetDAO.cs
@@ -0,0 +1,66 @@
+﻿using DBCovid.Data;
+using DBCovid.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PttkProject.DatabaseDAO
+{
+    public class TTTruyVetDAO: DBCovidContext
+    {
+        /*Thông tin truy vết*/
+        public bool themThongTinTruyVet(ThongTinTruyVet t)
+        {
+            try
+            {
+                thongTinTruyVet.Add(t);
+                SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public Object layDSThongTinTruyVet(int benhAnID)
+        {
+            //List< TTTruyVet >list;
+
+            var list = (from tt in thongTinTruyVet
+                        join x in xa
+                        on tt.xaID equals x.ID
+
+                        join h in huyen
+                        on x.huyenID equals h.ID
+                        join t in tinh
+                        on h.tinhID equals t.ID
+
+                        where tt.benhAnID == benhAnID
+                        select new
+                        {
+                            ID = tt.ID,
+                            thoiGian = tt.thoiGian,
+                            diaChiChiTiet = tt.diaChichiTiet,
+                            xa = x.tenXa,
+                            huyen = h.tenHuyen,
+                            tinh = t.tenTinh,
+                        }).ToList();
+            return list;
+        }
+        public bool xoaThongTinTruyVet(int ID)
+        {
+            try
+            {
+                ThongTinTruyVet t = thongTinTruyVet.Where(x => x.ID == ID).FirstOrDefault();
+                thongTinTruyVet.Remove(t);
+                SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
